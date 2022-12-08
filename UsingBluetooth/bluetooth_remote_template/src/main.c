363dfb4ebd133972b7957ccc74930bf0ab92525f
SEALBEAN <xuhet2017@gmail.com>
2022-12-08 14:59:45 +0700

    Update v2.1.0 SDK

--- UsingBluetooth/bluetooth_remote_template/src/main.c
+++ UsingBluetooth/bluetooth_remote_template/src/main.c
@@ -17,7 +17,6 @@ LOG_MODULE_REGISTER(LOG_MODULE_NAME);
 #define CONN_STATUS_LED DK_LED2
 #define RUN_LED_BLINK_INTERVAL 1000
 
-
 /* Declarations */
 
 void on_connected(struct bt_conn *conn, uint8_t err);
@@ -26,8 +25,6 @@ void on_notif_changed(enum bt_button_notifications_enabled status);
 void on_data_received(struct bt_conn *conn, const uint8_t *const data, uint16_t len);
 
 /* Callbacks */
-
-
 
 static struct bt_conn *current_conn;
 
@@ -42,25 +39,25 @@ struct bt_remote_service_cb remote_callbacks = {
 };
 
 /*
-	Call backs được chạy khi nhận được dữ liệu	
+	Call backs được chạy khi nhận được dữ liệu
 */
 void on_data_received(struct bt_conn *conn, const uint8_t *const data, uint16_t len)
 {
-	//Tạo String để lưu dữ liệu và chốt kí tự cuối là /0 để thông báo kết thúc String
+	// Tạo String để lưu dữ liệu và chốt kí tự cuối là /0 để thông báo kết thúc String
 	uint8_t temp_str[len + 1];
 	memcpy(temp_str, data, len);
 	temp_str[len] = 0x00;
 
 	LOG_INF("Received data on conn %p. Len: %d", (void *)conn, len);
-	LOG_INF("Data: %s", log_strdup(temp_str));
+	LOG_INF("Data: %s", temp_str);
 }
 
 /*
 	Call backs được chạy khi Central bật chế độ nhận Notify
 */
 void on_notif_changed(enum bt_button_notifications_enabled status)
-{  
-	if (status == BT_BUTTON_NOTIFICATIONS_ENABLED) 
+{
+	if (status == BT_BUTTON_NOTIFICATIONS_ENABLED)
 	{
 		LOG_INF("Notifications enabled");
 	}
@@ -81,7 +78,7 @@ void on_connected(struct bt_conn *conn, uint8_t err)
 		return;
 	}
 	LOG_INF("Connected.");
-					//Tăng biến đếm số lượng connect
+	// Tăng biến đếm số lượng connect
 	current_conn = bt_conn_ref(conn);
 	dk_set_led_on(CONN_STATUS_LED);
 }
@@ -95,10 +92,10 @@ void on_disconnected(struct bt_conn *conn, uint8_t reason)
 	dk_set_led_off(CONN_STATUS_LED);
 	if (current_conn)
 	{
-		//Giảm biến đếm số lượng connect 
+		// Giảm biến đếm số lượng connect
 		bt_conn_unref(current_conn);
-		//Nếu số lượng = 0 thì nó là NULL
-		//Ở đây chắc muốn chỉ kết nối được 1 thiết bị nên gán lại cho chắc
+		// Nếu số lượng = 0 thì nó là NULL
+		// Ở đây chắc muốn chỉ kết nối được 1 thiết bị nên gán lại cho chắc
 		current_conn = NULL;
 	}
 }
@@ -106,33 +103,33 @@ void on_disconnected(struct bt_conn *conn, uint8_t reason)
 void button_handler(uint32_t button_state, uint32_t has_changed)
 {
 	int button_pressed = 0;
-	//Giá trị của button_state và has_changed được các hàm khác truyền vào
-	//tương ứng với số bit của đèn tương ứng 0001  0010   0100  1000
+	// Giá trị của button_state và has_changed được các hàm khác truyền vào
+	// tương ứng với số bit của đèn tương ứng 0001  0010   0100  1000
 	if (has_changed & button_state)
 	{
 		switch (has_changed)
 		{
-			case DK_BTN1_MSK:
-				button_pressed = 1;
-				break;
-			case DK_BTN2_MSK:
-				button_pressed = 2;
-				break;
-			case DK_BTN3_MSK:
-				button_pressed = 3;
-				break;
-			case DK_BTN4_MSK:
-				button_pressed = 4;
-				break;
-			default:
-				break;
+		case DK_BTN1_MSK:
+			button_pressed = 1;
+			break;
+		case DK_BTN2_MSK:
+			button_pressed = 2;
+			break;
+		case DK_BTN3_MSK:
+			button_pressed = 3;
+			break;
+		case DK_BTN4_MSK:
+			button_pressed = 4;
+			break;
+		default:
+			break;
 		}
 		LOG_INF("Button %d pressed.", button_pressed);
 		set_button_value(button_pressed);
-			// Gọi hàm bên remote.c để in thông báo ra console
-			// Lưu ý data trên app đọc được sẽ bị ngược chiều
-															//Dữ liệu gửi	số byte
-			int err = send_button_notification(current_conn, button_pressed, 2);
+		// Gọi hàm bên remote.c để in thông báo ra console
+		// Lưu ý data trên app đọc được sẽ bị ngược chiều
+		// Dữ liệu gửi	số byte
+		int err = send_button_notification(current_conn, button_pressed, 2);
 		if (err)
 		{
 			LOG_ERR("couldn't send notification (err: %d)", err);
@@ -146,7 +143,7 @@ static void configure_dk_buttons_leds(void)
 {
 	int err;
 
-	//Thiết lập callbacks sẽ được thực hiện khi giá trị của button thay đổi
+	// Thiết lập callbacks sẽ được thực hiện khi giá trị của button thay đổi
 	err = dk_buttons_init(button_handler);
 	if (err)
 	{
@@ -170,13 +167,15 @@ void main(void)
 	configure_dk_buttons_leds();
 
 	err = bluetooth_init(&bluetooth_callbacks, &remote_callbacks);
-	if (err) {
+	if (err)
+	{
 		LOG_ERR("bt_enable returned %d", err);
 	}
 
 	LOG_INF("Running...");
-	for (;;) {
-		dk_set_led(RUN_STATUS_LED, (blink_status++%2));
+	for (;;)
+	{
+		dk_set_led(RUN_STATUS_LED, (blink_status++ % 2));
 		k_sleep(K_MSEC(RUN_LED_BLINK_INTERVAL));
 	}
 }
